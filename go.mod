4134cd338404109499c5afa3d6b2fab211ce43f9
Muninn <hyacinthus@gmail.com>
2021-02-25 20:51:32 +0800

    wip

--- go.mod
+++ go.mod
@@ -12,12 +12,15 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hack-fan/config v0.0.0-20200528030741-0a9be90586b6 // indirect
+	github.com/hack-fan/skadigo v0.0.0-20210123113312-a5627dfa396d // indirect
 	github.com/magefile/mage v1.11.0 // indirect
 	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.8.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
 	golang.org/x/sys v0.0.0-20210225014209-683adc9d29d7 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
--- main.go
+++ main.go
@@ -2,33 +2,62 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/hack-fan/skadigo"
+	"go.uber.org/zap"
+
+	"github.com/hack-fan/config"
 )
 
+var cli *client.Client
+var log *zap.SugaredLogger
+var ctx = context.Background()
+
+type Settings struct {
+	Debug  bool `default:"false"`
+	Token  string
+	Server string `default:"https://api.letserver.run"`
+	// default service name to restart
+	Default string
+}
+
+func handler(msg string) (string, error) {
+	resp, _, err := cli.ServiceInspectWithRaw(ctx, "server_api", types.ServiceInspectOptions{})
+	if err != nil {
+		panic(err)
+	}
+	return resp.Spec.Name, nil
+}
+
 func main() {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	var err error
+	// load config
+	var settings = new(Settings)
+	config.MustLoad(settings)
+
+	// logger
+	var logger *zap.Logger
+	if settings.Debug {
+		logger, err = zap.NewDevelopment()
+	} else {
+		logger, err = zap.NewProduction()
+	}
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync() // nolint
+	log = logger.Sugar()
+
+	// docker cli
+	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	resp, raw, err := cli.ServiceInspectWithRaw(ctx, "server_api", types.ServiceInspectOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	data, err := json.Marshal(resp)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
-	fmt.Println(string(raw))
-
-	time.Sleep(time.Minute)
+	// skadi agent
+	agent := skadigo.New(settings.Token, settings.Server, handler, nil)
+	log.Info("Skadi agent start")
+	agent.Start()
 }
